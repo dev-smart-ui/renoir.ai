4d50b15fe041e5374a13612894c1ca74cd5cfd03
dev-smart-ui <dev@smart-ui.pro>
2025-03-07 18:18:10 +0200

    add favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,14 +1,17 @@
 import "./globals.css"
 import { Inter } from "next/font/google"
 import type { Metadata } from "next"
-import type React from "react" // Added import for React
+import type React from "react"
 
 const inter = Inter({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
   title: "Renoir.AI - AI-Powered Sales & Revenue Automation",
   description: "Streamline growth, optimize efficiency, and close more deals with AI-driven workflows.",
-    generator: 'v0.dev'
+  generator: 'v0.dev',
+  icons: {
+    icon: '/favicon/favicon.ico',
+  },
 }
 
 export default function RootLayout({
@@ -18,11 +21,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <body className={inter.className}>{children}</body>
     </html>
   )
 }
-
 
 
 import './globals.css'
